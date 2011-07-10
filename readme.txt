747311ea1db703646b2f2a1928d55bf50c5d289a
onnayokheng <onnayokheng@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-10 02:47:58 +0000

    Upgrade plugin, adding plugin options
    git-svn-id: https://plugins.svn.wordpress.org/wpec-related-products/trunk@407686 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,11 +1,11 @@
-=== WPEC Related Products ===
+=== WP e-Commerce Related Products ===
 Contributors: Onnay Okheng
 Tags: wp e commerce, wordpress, plugin, spec, wpsc related products, related product,
 Requires at least: 3.0
-Tested up to: 3.1.3
-Stable Tag: 1.0
+Tested up to: 3.2
+Stable Tag: 1.1
 
-Ratings: clean and easy
+WPEC Related Products for WP e-Commerce uses information available within the Single Product template to display related Products.
 
 == Description ==
 
@@ -13,7 +13,24 @@ WPEC Related Products for WP e-Commerce uses information available within the Si
 
 WPEC Related Products is a WordPress Plugin to extend the WP e-Commerce Plugin by displaying related Products to site visitors, this provides a cross-selling opportunity to other Products available from your store. It automatically selects related Products based on the Category that the currently displayed Product belongs to.
 
+
+Contact me for say hello <a href="http://onnayokheng.com">Onnay Okheng</a> or buy me a cup of chocolate :)
+
+== Installation ==
+
+1. Upload the folder 'WP e-Commerce Related Products' to the '/wp-content/plugins/' directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
 == Changelog ==
 
-= 1.0 =
-* release
+<h4>1.0</h4>
+<ul>
+    <li>Edit: Change the plugin name.</li>
+    <li>New: Add Plugin Options</li>
+    <li>New: Can setting number, title, and image for display.</li>
+</ul>
+
+<h4>1.0</h4>
+<ul>
+    <li>Initial release</li>
+</ul>
--- wpec-related-product.php
+++ wpec-related-product.php
@@ -1,29 +1,61 @@
 <?php
 /**
-  * Plugin Name: WP e-Commerce Related Products
+  * Plugin Name: WP e-Commerce Related Product
   * Description: WPEC Related Products for WP e-Commerce uses information available within the Single Product template to display related Products that belong to the same Product Category.
-  * Version: 1.0
+  * Version: 1.1
   * Author: Onnay Okheng
   * Author URI: http://onnayokheng.com/
+
+    Copyright (C) 2010-2010, Onnay Okheng
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    Neither the name of Alex Moss or pleer nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
   **/
+
+
+// Checking page if is admin page
+if(is_admin()){
+    
+    // call function for admin menu
+    add_action('admin_menu', 'on_wpec_options');
+    
+    // function for google +1 options
+    function on_wpec_options(){
+        
+        // add a new setting submenu
+        add_options_page('WPEC Related Products', 'WPEC Related Products', 'manage_options', 'wpec-related-products', 'on_wpec_panel');        
+        
+    }
+       
+}
 
 /**
  * Function for displaying the related products
  *
  * @global type $post 
  */
-function wpec_related(){
+function on_wpec_related(){
     global $post;
     
+        // get related from produt category.
         $product_cat = wp_get_object_terms(wpsc_the_product_id(), 'wpsc_product_category');
         
         foreach ($product_cat as $cat_item) {
             $cat_array_name_list[] = $cat_item->slug;
         }
+        
+        $number = (get_option('on_wpec_number') == '')? 4: get_option('on_wpec_number');
+        $title  = (get_option('on_wpec_title') == '')? 'Related Products': get_option('on_wpec_title');
 
         if (empty($related_product)) {
              $query = array (
-                'showposts' => 4,
+                'showposts' => $number,
                 'orderby'   => 'rand',
                 'post_type' => 'wpsc-product',
                 'tax_query' => array(
@@ -40,7 +72,7 @@ function wpec_related(){
             if(!$related_product->have_posts()){
 
                  $query = array (
-                    'showposts' => 4,
+                    'showposts' => $number,
                     'orderby'   => 'rand',
                     'post_type' => 'wpsc-product',
                     'post__not_in' => array ($post->ID),
@@ -52,13 +84,14 @@ function wpec_related(){
                 
                 echo "<div class='wpec-related-wrap'>";
             
-                echo "<h2>".__('Related Products')."</h2>";
+                echo "<h2>".$title."</h2>";
                 
                 while($related_product->have_posts()) : $related_product->the_post();
             ?>
 
                     <div class="wpec-related-product product-<?php echo wpsc_the_product_id(); ?> <?php echo wpsc_category_class(); ?>">
 
+                        <?php if(get_option('on_wpec_image') == 'on') : ?>
                             <div class="wpec-related-image" id="related-pro-<?php echo wpsc_the_product_id(); ?>">
                                     <a href="<?php echo wpsc_the_product_permalink(); ?>">
 
@@ -69,6 +102,7 @@ function wpec_related(){
                                         <?php endif; ?>
                                     </a>
                             </div><!--close imagecol-->
+                        <?php endif; ?>
 
                             <h3 class="wpec-related-title">
                                     <?php if(get_option('hide_name_link') == 1) : ?>
@@ -79,9 +113,11 @@ function wpec_related(){
                             </h3>
 
 
+                        <?php if(get_option('on_wpec_price') == 'on') : ?>
                             <div class="product-info">
                                     <div class="pricedisplay <?php echo wpsc_the_product_id(); ?>"><?php _e('Price', 'wpsc'); ?>: <span id='product_price_<?php echo wpsc_the_product_id(); ?>' class="currentprice pricedisplay"><?php echo wpsc_the_product_price(); ?></span></div>
                             </div>
+                        <?php endif; ?>
 
                     </div><!-- close default_product_display -->
 
@@ -96,7 +132,7 @@ function wpec_related(){
         
 }
 
-function wpec_related_style(){
+function on_wpec_related_style(){
 ?>
         <style>
             .wpec-related-wrap{margin: 20px 0; padding: 0; display: inline-block;}
@@ -107,7 +143,67 @@ function wpec_related_style(){
 <?php
 }
 
-add_action('wpsc_product_addon_after_descr', 'wpec_related');
-add_action('wp_head','wpec_related_style');
+add_action('wpsc_product_addon_after_descr', 'on_wpec_related');
+add_action('wp_head','on_wpec_related_style');
 
-?>
+
+/**
+ * Function for display the Plugin Panel Options.
+ */
+function on_wpec_panel() { ?>
+
+<div class="wrap">
+    <div id="icon-options-general" class="icon32"><br></div>
+    <h2>WPEC Related Products Options</h2>
+
+    <form method="post" action="options.php" id="options">
+    <?php wp_nonce_field('update-options') ?>
+                
+        <table class="form-table">
+            <tbody>
+
+                <tr valign="top">
+                    <th scope="row">Title</th>
+                    <td>
+                            <input type="text" name="on_wpec_title" value="<?php echo get_option('on_wpec_title'); ?>" />
+                            <br/>Default is "Related Products".
+                    </td>
+                </tr>
+
+                <tr valign="top">
+                    <th scope="row">Number of related products</th>
+                    <td>
+                            <input type="text" name="on_wpec_number" value="<?php echo get_option('on_wpec_number'); ?>" />
+                            <br/>Default is 4.
+                    </td>
+                </tr>
+
+                <tr valign="top">
+                    <th scope="row">Show image</th>
+                    <td>                        
+                            <?php $checked_image = (get_option('on_wpec_image') == 'on') ? ' checked="yes"' : ''; ?>                    
+                            <label id="on_wpec_image" ><input type="checkbox" id="on_wpec_image" name="on_wpec_image"<?php echo $checked_image; ?> /> Enabled / Disabled</label>                                    
+                    </td>
+                </tr>
+
+                <tr valign="top">
+                    <th scope="row">Show price</th>
+                    <td>                        
+                            <?php $checked_price = (get_option('on_wpec_price') == 'on') ? ' checked="yes"' : ''; ?>                    
+                            <label id="on_wpec_price" ><input type="checkbox" id="on_wpec_price" name="on_wpec_price"<?php echo $checked_price; ?> /> Enabled / Disabled</label>                                    
+                    </td>
+                </tr>
+
+            </tbody>
+        </table>
+        
+        <input type="hidden" name="action" value="update" />
+        <input type="hidden" name="page_options" value="on_wpec_image, on_wpec_number, on_wpec_title, on_wpec_price" />
+        <div class="submit"><input type="submit" class="button-primary" name="submit" value="Save Settings"></div>
+
+
+    </form>
+
+</div>
+
+<?php } ?>
